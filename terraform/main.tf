0a546f09b1243c5a2cdc675655cd7e47f35878b7
gaurav madaan <gauravmadaan143@gmail.com>
2025-07-10 00:05:09 +0200

    updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "24_04-lts"
+    sku       = "20_04-lts"
     version   = "latest"
   }
 }
