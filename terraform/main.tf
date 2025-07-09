49a3259281c30bbed18640f8a0613fb26243d48b
gaurav madaan <gauravmadaan143@gmail.com>
2025-07-09 22:47:12 +0200

    updated main.tf and yaml file

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,12 +28,12 @@ resource "azurerm_virtual_network" "main" {
   name                = "demo-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "main" {
   name                 = "demo-subnet"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.1.0/24"]
 }
@@ -41,7 +41,7 @@ resource "azurerm_subnet" "main" {
 resource "azurerm_network_security_group" "main" {
   name                = "demo-nsg"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.main.name
 
   security_rule {
     name                       = "Allow-SSH"
@@ -64,14 +64,14 @@ resource "azurerm_subnet_network_security_group_association" "main" {
 resource "azurerm_public_ip" "main" {
   name                = "demo-public-ip"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "demo-nic"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.main.name
 
   ip_configuration {
     name                          = "internal"
@@ -83,12 +83,12 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_linux_virtual_machine" "main" {
   name                = var.vm_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.main.name
   location            = var.location
   size                = "Standard_B1s"
   admin_username      = "azureuser"
 
-  network_interface_ids          = [azurerm_network_interface.main.id]
+  network_interface_ids           = [azurerm_network_interface.main.id]
   disable_password_authentication = true
 
   admin_ssh_key {
